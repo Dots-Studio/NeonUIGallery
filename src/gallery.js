290429e24f615ad37985f5655986aa8772aec8c0
AlurDesign <93062699+AlurDesign@users.noreply.github.com>
2022-01-19 02:29:17 +0100

    First release

---
+++ src/gallery.js
@@ -0,0 +1,20 @@
+//This JS file is just used for the NeonUI controls gallery.
+//In your app, use NeonUI.js
+
+var sheet = document.querySelector("#gallerySheet")
+var dialog = document.querySelector("#galleryDialog")
+
+function randomValue() {
+    var RandomNumber = Math.floor(Math.random() * 101);
+    var ProgressBar = document.getElementById("galleryProgress")
+    ProgressBar.style.width = RandomNumber + "%"
+}
+
+function ToggleVisibility(e) {
+  if(getComputedStyle(e, null).display == 'none') {
+    e.style.display = 'flex'
+  }
+  else {
+    e.style.display = 'none'
+  }
+}
